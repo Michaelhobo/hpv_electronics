e4e43df5e559d28a71758baeb8fdef4604d089ca
Michael Ho <callmemikeh@gmail.com>
2015-03-29 16:17:08 -0700

    First rewrite of master receiver

--- master_receiver/main.cpp
+++ master_receiver/main.cpp
@@ -10,93 +10,93 @@
 #define PC_SEND_INTERVAL 1
 #define MY_ADDR 0
 
+/* Sensor count goes like this:
+0 - speed
+1 - cadence
+2 - temp
+3 - outdoor brightness
+*/
+#define NUM_SENSORS 4
+
+
+/* Actuator count goes like this:
+0 - gear shifter
+1 - front light
+2 - rear lights
+3 - landing gear
+*/
+#define NUM_ACTUATORS 4
+
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 DigitalOut led4(LED4);
 Serial pc(USBTX, USBRX); // tx, rx
 xbee xbee(p13, p14, p12);
-nRF24L01P rf24(p5, p6, p7, p8, p9, p10);
-
-InterruptIn button(p15);
+I2C arduino(p9, p10);
+InterruptIn critical(p8);
 
 Ticker events;
 Timeout timeout;
+
+/* I2C values */
+uint8_t read_addr = 0x7 << 1 | 0x1;
+uint8_t write_addr = 0x7 << 1;
+char out[2];
+char arduino_updates[NUM_SENSORS];
+
+/* Data Values. */
 double speed = 0.0; //calculated speed
-double cadence = 0.0;
-char receive_buffer[RF24_TRANSFER_SIZE];
-char speed_buffer[RF24_TRANSFER_SIZE];
-char send_buffer[RF24_TRANSFER_SIZE];
-const char *sensor_names[255] = {0};
-void (*sensor_handlers[255])(char *data);
-uint8_t sensor_states[255];
-
-/* Sends speed to XBEE. */
-void send_xbee_speed() {
-	sprintf(speed_buffer, "%f\n", speed);
-}
-
-/* Initialize xbee for telemetry. */
-void telemetry_init() {
-	// convert units before to minimize calculations in critical section
-	pc.printf("Initializing.\n");
-}
-
-/* Initialize one sensor's values. */
-void init_sensor(int id, const char *name, void (*handler)(char *)) {
-	sensor_names[id] = name;
-	sensor_handlers[id] = handler;
-	sensor_states[id] = (uint8_t) DISCONNECTED;
-}
+uint8_t cadence = 0;
+int8_t temp = 0;
+uint8_t brightness = 0;
 
 
-void send_one() {
-	pc.printf("h sent\r\n");
-	uint8_t sent = send_sensor(1, "h");
-	pc.printf("send %d\r\n", sent);
-	//rf24.setTxAddress(0x01F0F0F0F1, 5);
-}
+/* Actuator Values */
+uint8_t gear = 1;
+bool front_light = false;
+bool rear_light = false;
+bool landing_gear = false;
 
 /* Initialize everything necessary for the scripts. */
 void init() {
-	pc.printf("init");
-	telemetry_init();
-	rf24_init();
-	button.rise(&send_one);
+	pc.printf("Human Powered Vehicle Controller");
+	critical.rise(&get_updates);
+}
+
+/* Get updates from arduino. 
+ * A '-' means the value has not been updated from last time.
+ */
+void get_updates() {
+	arduino.read(read_addr, arduino_updates, NUM_SENSORS);
+	for (int i = 0; i < NUM_SENSORS; i++) {
+		if (arduino_updates[i] != '-') {
+			switch (i) {
+				case 0:
+					speed = (double) ((uint8_t) arduino_updates[i] * 3.0 / 10.0); // speed converion 
+					break;
+				case 1:
+					cadence = arduino_updates[i];
+					break;
+				case 2:
+					temp = arduino_updates[i];
+					break;
+				case 3:
+					brightness = arduino_updates[i];
+					break;
+			}
+		}
+	}
 }
 
 /* Send to a sensor with an id. */
-uint8_t send_sensor(uint8_t id, char *data) {
-	pc.printf("send_sensor\r\n");
-	send_buffer[0] = id;
-	sprintf(send_buffer + 1, "%s", data);
-	uint64_t pipe_addr = 0x01F0F0F0F1;//0x00F0F0F0F1 | (1LL << 32);
-	num_received = rf24.write(NRF24L01P_PIPE_P0, send_buffer, RF24_TRANSFER_SIZE);
-	rf24.setReceiveMode();
-	pc.printf("sent.\r\n");
-	return num_received;
-}
-
-/* Gets the id from the name of the sensor. */
-uint8_t find_id(char *name) {
-	uint8_t id = 0;
-	while (strcmp(sensor_names[id], name)) {
-		id += 1;
-	}
-	return id;
-}
-
-/* Send to a sensor with name.
- * Sending format: [dest_address(1), src_address(1), data(30)]
- */
-void send_sensor_name(char *name, char *data) {
-	uint8_t id = find_id(name);
-	send_sensor(id, data);
+void send_sensor(uint8_t id, char data) {
+	out[0] = id;
+	out[1] = data;
+	arduino.write(write_addr, out, 2);
 }
 
 /* Main. */
 int main() {
 	init();
-	//pc.printf("Starting Logging.\n");
-	//events.attach(&send_xbee_speed, XBEE_SEND_INTERVAL);
 }
