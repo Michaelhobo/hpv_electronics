9b1a5784e024272ab9b33dd3885d76932348e276
Michael Ho <callmemikeh@gmail.com>
2015-03-28 21:22:37 -0700

    i2c working! I need to get some things together still, but first commit so I don't forget

--- I2CCode/I2CCode-Arduino/I2CCode-Arduino.ino
+++ I2CCode/I2CCode-Arduino/I2CCode-Arduino.ino
@@ -3,10 +3,12 @@
 const int TRANSFER_SIZE = 4;
 char data[TRANSFER_SIZE];
 void setup() {
-  Wire.begin(3);
+  Wire.begin(7);
   Wire.onReceive(receiveEvent);
+  Wire.onRequest(receiveRequest);
   Serial.begin(9600);
   pinMode(10, OUTPUT);
+  Serial.println("setup done");
 }
 
 void loop() {
@@ -14,18 +16,18 @@ void loop() {
 
 }
 
-
-char key[] = "ABCDE"; //Sensor keys
-
 void sendToSensor(char sensor, char* data){
 }
 
 //When the Arduino receives data from master, gets called
 void receiveEvent(int dataSize){
+  Serial.println("got smthn");
   int counter= 0;
   digitalWrite(10, LOW);
    while (Wire.available()&& counter < TRANSFER_SIZE){ //Reads data
       data[counter] =  Wire.read(); 
+      Serial.print(data[counter]);
+      //Serial.print("h");
       counter++;
    }
    if (data[0]== 'S'){ //Case for sending data to a different sensor
@@ -35,10 +37,20 @@ void receiveEvent(int dataSize){
    {
        if (data[1] == 'S'){//Speed
             Wire.write(10);
+            Serial.print("request speed");
        }
       else if (data[1]== 'C'){
             Wire.write(20);
       }
       digitalWrite(10, HIGH);
     }
+}
+
+void receiveRequest() {
+    Serial.println("got request");
+    data[0] = 0;
+    data[1] = 1;
+    data[2] = 2;
+    data[3] = 3;
+    Wire.write(data, TRANSFER_SIZE);
 }
