4cf15557835a492dc99e1a4a65160d4fcdeb3939
VoidShredder <rmomoi@berkeley.edu>
2015-04-03 19:11:03 -0700

    Minor change

--- sensor_template/sensor_template.ino
+++ sensor_template/sensor_template.ino
@@ -37,14 +37,14 @@ bool write_data() {
 	/* Write code here. */
 	rf24.stopListening();
 	bool received = false;
-		received = rf24.write(write_buffer, sizeof(char) * 10);
-		if (received) {
-			Serial.println("write ok...\n\r"); 
-		} else  {
-			Serial.println("write failed.\n\r");
-		}
-		delay(100);
-		rf24.startListening();
+	received = rf24.write(write_buffer, sizeof(char) * (RF24_TRANSFER_SIZE+1));
+	if (received) {
+		Serial.println("write ok...\n\r"); 
+	} else  {
+		Serial.println("write failed.\n\r");
+	}
+	delay(100);
+	rf24.startListening();
 	return received;
 }
 
