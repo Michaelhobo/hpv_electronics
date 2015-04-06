0eaa29cec0404b66d7c11fd5c9b45a668b8fc0e0
Michael Ho <callmemikeh@gmail.com>
2015-04-05 18:07:27 -0700

    update sensor script updated

--- update_sensors.sh
+++ update_sensors.sh
@@ -1,2 +1,4 @@
 SENSORS=sensor_*
 for sensor in $SENSORS; do cp sensor_template/sensor_template.ino $sensor/$sensor.ino; git add $sensor/$sensor.ino; cp sensor_template/constants.h $sensor/constants.h; git add $sensor/constants.h; done
+ACTUATORS=actuator_*
+for actuator in $ACTUATORS; do cp actuator_template/actuator_template.ino $actuator/$actuator.ino; git add $actuator/$actuator.ino; cp actuator_template/constants.h $actuator/constants.h; git add $actuator/constants.h; done
