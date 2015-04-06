09e5cca84f183c34191e141d976cb902eb603b1b
Michael Ho <callmemikeh@gmail.com>
2015-04-05 17:39:48 -0700

    testing new master radio code

--- master_radio/master_radio/master_radio.ino
+++ master_radio/master_radio/master_radio.ino
@@ -17,8 +17,10 @@ RF24 radio(8, 7);
 #define NUM_SENSORS 4
 
 const uint64_t rf24_addr = 0x00F0F0F0F0LL;
+const uint64_t base_addr = 0xF0F0F0F000LL;
 char sensor_data[NUM_SENSORS];
-char rf24_buffer[RF24_TRANSFER_SIZE];
+char rf24_in[RF24_TRANSFER_SIZE];
+char rf24_out[RF24_TRANSFER_SIZE];
 
 void setup(void)
 {
@@ -39,13 +41,24 @@ void setup(void)
   radio.startListening();
 }
 
-/* Send data to slave.
- * Called when arduino receives data over I2C
- */
+/* Receive data over I2C and deal with data accordingly. */
 void on_receive(int dataSize) {
 	uint8_t id = (uint8_t) Wire.read();
 	char data = Wire.read();
-	//send_slave(id, data);
+	send_slave(id, data);
+}
+
+void send_slave(uint8_t id, char data) {
+  rf24_out[0] = id;
+  rf24_out[1] = data;
+  radio.stopListening();
+  radio.openWritingPipe(base_addr | id);
+  bool ok = radio.write(rf24_out, RF24_TRANSFER_SIZE);
+  if (ok)
+    Serial.println("ok...");
+  else
+    Serial.println("failed.");
+  radio.startListening();
 }
 
 /* Send an update back to the master. */
@@ -61,31 +74,41 @@ void send_update() {
 void loop(void)
 {
     // if there is data ready
-    if ( radio.available() )
-    {
-      bool done = false;
-      while (!done)
-      {
-        // Fetch the payload, and see if this was the last one.
-        done = radio.read(rf24_buffer, RF24_TRANSFER_SIZE);
-        // Spew it
-        Serial.print("Got payload ...");
-        Serial.print(rf24_buffer);
+  if ( radio.available() )
+  {
+    radio.read(rf24_in, RF24_TRANSFER_SIZE);
+    // Spew it
+    Serial.print("Got payload ...");
+    Serial.print(rf24_in);
+    switch (rf24_in[0]) {
+      case 0: //speed
+        sensor_data[0] = rf24_in[1];
+        Serial.println("from speed sensor");
+        break;
+      case 1: //cadence
+        sensor_data[1] = rf24_in[1];
+        Serial.println("from cadence sensor");
+        break;
+      case 2: //temp
+        sensor_data[2] = rf24_in[1];
+        Serial.println("from temperature sensor");
+        break;
+      case 3: //brightness
+        Serial.println("from brightness sensor");
+        sensor_data[3] = rf24_in[1];
+        break;
 
-	// Delay just a little bit to let the other unit
-	// make the transition to receiver
-      delay(20);
+      case 'g': //gear shift
+        break;
+      case 'f': //front light
+        break;
+      case 'r': //right turn
+        break;
+      case 't': //left turn
+        break;
+      case 'l': //landing gear
+        break;
     }
-
-    // First, stop listening so we can talk
-    radio.stopListening();
-
-    // Send the final one back.
-    radio.write( rf24_buffer, RF24_TRANSFER_SIZE );
-    Serial.println("...Sent response.\n\r");
-
-    // Now, resume listening so we catch the next packets.
-    radio.startListening();
   }
 }
 // vim:cin:ai:sts=2 sw=2 ft=cpp
