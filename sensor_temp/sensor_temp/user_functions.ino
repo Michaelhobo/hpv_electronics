e0e46640fc286a2f6c3859343e849a33af10706c
harrisonhsueh <harrison.hsueh@berkeley.edu>
2015-04-08 22:41:22 -0700

    sensor temp not working

---
+++ sensor_temp/sensor_temp/user_functions.ino
@@ -0,0 +1,86 @@
+/* User Functions - Sensor Template
+ * These functions should perform appropriate actions at every cycle of the loop.
+ * Collected and formatted data should be stored in write_buffer[1]
+ * This file is meant to pull user-defined functions out of the base template,
+ * so the template can change without fear of merge conflicts.
+ */
+#include <dht11.h>
+dht11 DHT11;
+#define DHT11PIN 2
+int i = 0;
+double Fahrenheit(double celsius)
+{
+	return 1.8 * celsius + 32;
+}
+
+//Celsius to Kelvin conversion
+double Kelvin(double celsius)
+{
+	return celsius + 273.15;
+} 
+/* Allows user to set things up. */
+void user_setup() {
+  //Celsius to Fahrenheit conversion
+  
+  Serial.begin(115200);
+  Serial.println("DHT11 TEST PROGRAM ");
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(DHT11LIB_VERSION);
+  Serial.println();
+}
+
+/* This function is called before we send. */
+void data_manipulation() {
+  Serial.println("\n");
+
+  int chk = DHT11.read(DHT11PIN);
+
+  Serial.print("Read sensor: ");
+  switch (chk)
+  {
+    case DHTLIB_OK: 
+		Serial.println("OK"); 
+		break;
+    case DHTLIB_ERROR_CHECKSUM: 
+		Serial.println("Checksum error"); 
+		break;
+    case DHTLIB_ERROR_TIMEOUT: 
+		Serial.println("Time out error"); 
+		break;
+    default: 
+		Serial.println("Unknown error"); 
+		break;
+  }
+  
+  if (i == 1) {
+    Serial.print("Humidity (%): ");
+    Serial.println((float)DHT11.humidity, 2);
+    write_buffer[1] = (uint8_t) DHT11.humidity;
+    i = 0;
+    
+  } else {
+    Serial.print("Temperature (°C): ");
+    Serial.println((float)DHT11.temperature, 2);
+    
+    Serial.print("Temperature (°F): ");
+    Serial.println(Fahrenheit(DHT11.temperature), 2);
+
+    Serial.print("Temperature (°K): ");
+    Serial.println(Kelvin(DHT11.temperature), 2);
+    write_buffer[1] = (uint8_t) DHT11.temperature;
+    i = 1;
+  }
+
+ 
+  delay(2000);
+}
+
+/* These functions are called after we send data,
+ * and we're figuring out what state 
+ * we're going to switch to.
+ */
+void connected_action() {}
+
+void sleep_action() {}
+
+void deep_sleep_action() {}
